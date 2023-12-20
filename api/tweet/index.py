07f6f97db560ab0a23e9b16258070664cc16a285
Julian de Mourgues <juliandemourgues@gmail.com>
2023-12-20 19:00:36 +0100

    add tweet

--- api/tweet/index.py
+++ api/tweet/index.py
@@ -1,6 +1,7 @@
 import tweepy
 
 def tweet(text: str, media: str = None):
+    print("Tweeting: ", text, media)
     api = api()
     if media:
         media_ids = [api.media_upload(media).media_id_string]
@@ -8,13 +9,13 @@ def tweet(text: str, media: str = None):
     return api.update_status(text)
 
 def api():
-    consumer_key = os.environ.get("CONSUMER_KEY")
-    consumer_secret = os.environ.get("CONSUMER_SECRET")
+    consumer_key = os.environ.get("API_KEY")
+    consumer_secret = os.environ.get("API_SECRET")
     access_token = os.environ.get("ACCESS_TOKEN")
     access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
     if not consumer_key or not consumer_secret or not access_token or not access_token_secret:
-        raise Exception("Please set the environment variables: CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET")
+        raise Exception("Please set the environment variables: API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET")
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -23,5 +24,8 @@ def api():
     return client
 
 def handler(inputs):
-    tweet(inputs.message, inputs.media)
-    return inputs.message
+    for key in inputs.messages:
+        tweet(inputs.messages[key], inputs.mediaPaths[key])
+    return {
+        success: True
+    }
