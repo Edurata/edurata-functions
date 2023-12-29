2ebc0e8b11a6947e7dd9a7748135eeb60bc386d4
Julian de Mourgues <juliandemourgues@gmail.com>
2023-12-30 00:26:03 +0100

    change

--- etl/extract/fetch-telegram/index.py
+++ etl/extract/fetch-telegram/index.py
@@ -1,14 +1,14 @@
 import asyncio
-from telethon import TelegramClient
+from telethon.sync import TelegramClient
 import os
 import logging
 from telethon.tl.types import MessageMediaPhoto
 from datetime import datetime, timedelta, timezone
-
+from telethon.sessions import StringSession
 # Replace with your own credentials
 api_id = os.environ.get('TELEGRAM_API_ID')
 api_hash = os.environ.get('TELEGRAM_API_HASH')
-
+session = os.environ.get('TELEGRAM_API_SESSION') 
 async def get_posts(client, channel_id, since_days, image_dir, limit=100):
     # Directory to save images
     os.makedirs(image_dir, exist_ok=True)
@@ -36,25 +36,31 @@ async def get_posts(client, channel_id, since_days, image_dir, limit=100):
     return messages
 
 def handler(inputs):
+    print("Starting the function")
     # Initialize the client
-    client = TelegramClient('main', api_id, api_hash)
-    channel_id = inputs["channelId"]
-    since_days = inputs["sinceDays"]
-    image_dir = inputs["imageDir"]
-    loop = asyncio.get_event_loop()
-    print(f"Fetching posts from {channel_id} since {since_days} days ago")
-    posts = loop.run_until_complete(get_posts(client, channel_id, since_days, image_dir))
+    with TelegramClient(StringSession(session), api_id, api_hash) as client:
+        if (session == None):
+            # If no session is provided, generate a new one
+            print("Generating new session. Save this to your environment variables")
+            print(client.session.save())
 
-    return {
-        "posts": posts,
-        "mediaPaths": "downloaded_images/*"
+        channel_id = inputs["channelId"]
+        since_days = inputs["sinceDays"]
+        image_dir = inputs["imageDir"]
+        loop = asyncio.get_event_loop()
+        print(f"Fetching posts from {channel_id} since {since_days} days ago")
+        posts = loop.run_until_complete(get_posts(client, channel_id, since_days, image_dir))
+
+        return {
+            "posts": posts,
+            "mediaPaths": "downloaded_images/*"
+        }
+
+if __name__ == '__main__':
+    # For local testing
+    inputs = {
+        "channelId": "knVknVknV",
+        "sinceDays": 3,
+        "imageDir": "downloaded_images"
     }
-
-# if __name__ == '__main__':
-#     # For local testing
-#     inputs = {
-#         "channelId": "knVknVknV",
-#         "sinceDays": 3,
-#         "imageDir": "downloaded_images"
-#     }
-#     handler(inputs)
+    handler(inputs)
