43ece8c0926ecbbe8ff80ec429474a335e56409b
Julian de Mourgues <juliandemourgues@gmail.com>
2024-01-04 10:35:35 +0100

    translate

--- etl/transform/translate-deepl/dist/tsc/index.js
+++ etl/transform/translate-deepl/dist/tsc/index.js
@@ -32,29 +32,29 @@ const handler = async (inputs) => {
         throw new Error("No Deepl API key found");
     }
     const translator = new deepl.Translator(apiKey);
-    // Filter out empty elements from sourceTexts and keep track of their positions
-    const nonEmptyTexts = [];
-    const emptyIndices = [];
+    // Filter out empty and null elements from sourceTexts and keep track of their positions
+    const nonEmptyOrNullTexts = [];
+    const emptyOrNullIndices = [];
     for (let i = 0; i < sourceTexts.length; i++) {
-        if (sourceTexts[i].trim() !== "") {
-            nonEmptyTexts.push(sourceTexts[i]);
+        if (sourceTexts[i] !== null && sourceTexts[i].trim() !== "") {
+            nonEmptyOrNullTexts.push(sourceTexts[i]);
         }
         else {
-            emptyIndices.push(i);
+            emptyOrNullIndices.push(i);
         }
     }
-    // Translate non-empty texts
-    const translationResult = await translator.translateText(nonEmptyTexts, null, targetLanguage);
-    // Re-insert empty elements at the correct positions in the response
+    // Translate non-empty/non-null texts
+    const translationResult = await translator.translateText(nonEmptyOrNullTexts, null, targetLanguage);
+    // Re-insert empty/null elements at the correct positions in the response
     const translations = [];
-    let emptyIndex = 0;
+    let emptyOrNullIndex = 0;
     for (let i = 0; i < sourceTexts.length; i++) {
-        if (emptyIndices.includes(i)) {
-            translations.push(""); // Insert empty string
+        if (emptyOrNullIndices.includes(i)) {
+            translations.push(sourceTexts[i]); // Insert empty or null element
         }
         else {
-            translations.push(translationResult[emptyIndex].text);
-            emptyIndex++;
+            translations.push(translationResult[emptyOrNullIndex].text);
+            emptyOrNullIndex++;
         }
     }
     return {
