3eb2c58fd1700159ab7e6b070e086834cb2c11a3
Julian de Mourgues <juliandemourgues@gmail.com>
2024-01-04 11:05:26 +0100

    translate

--- etl/load/tweet/index.py
+++ etl/load/tweet/index.py
@@ -1,13 +1,5 @@
 import tweepy
 
-def tweet(text: str, media: str = None):
-    print("Tweeting: ", text, media)
-    api = api()
-    if media:
-        media_ids = [api.media_upload(media).media_id_string]
-        return api.update_status(text, media_ids=media_ids)
-    return api.update_status(text)
-
 def api():
     consumer_key = os.environ.get("API_KEY")
     consumer_secret = os.environ.get("API_SECRET")
@@ -22,6 +14,14 @@ def api():
 
     client = tweepy.API(auth)
     return client
+
+def tweet(text: str, media: str = None):
+    print("Tweeting: ", text, media)
+    api = api()
+    if media:
+        media_ids = [api.media_upload(media).media_id_string]
+        return api.update_status(text, media_ids=media_ids)
+    return api.update_status(text)
 
 def handler(inputs):
     for key in inputs.messages:
