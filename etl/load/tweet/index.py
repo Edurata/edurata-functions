0d1bd9950c8d95bab54a39c68db8175d41f75489
Julian de Mourgues <juliandemourgues@gmail.com>
2024-01-04 21:49:47 +0100

    api v2

--- etl/load/tweet/index.py
+++ etl/load/tweet/index.py
@@ -1,38 +1,50 @@
 import tweepy
 import os
 
-def get_api():
-    consumer_key = os.environ.get("API_KEY")
-    consumer_secret = os.environ.get("API_SECRET")
-    access_token = os.environ.get("ACCESS_TOKEN")
-    access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+consumer_key = os.environ.get("API_KEY")
+consumer_secret = os.environ.get("API_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+bearer_token = os.environ.get("BEARER_TOKEN")
+if not consumer_key or not consumer_secret or not access_token or not access_token_secret:
+    raise Exception("Please set the environment variables: API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET")
 
-    if not consumer_key or not consumer_secret or not access_token or not access_token_secret:
-        raise Exception("Please set the environment variables: API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET")
 
+def get_api_v1():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     client = tweepy.API(auth)
     return client
 
+def get_api_v2():
+    client = api = tweepy.Client(bearer_token=bearer_token,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret)
+
+    return client
+
 def tweet(text: str, media: str = None):
     print("Tweeting: ", text, media)
-    api = get_api()
+    apiV1 = get_api_v1()
+    apiV2 = get_api_v2()
+    media_ids = []
     if media:
-        media_ids = [api.media_upload(media).media_id_string]
-        return api.update_status(text, media_ids=media_ids)
-    return api.update_status(text)
+        media_ids = [apiV1.media_upload(media).media_id_string]
+    # return apiV1.update_status(text, media_ids=media_ids)
+    return apiV2.create_tweet(text=text,media_ids=media_ids)
 
 def handler(inputs):
     for index, element in enumerate(inputs["messages"]):
         tweet(inputs["messages"][index], inputs["mediaPaths"][index])
     return {
-        success: True
+        "success": True
     }
 
 if __name__ == "__main__":
     handler({
         "messages": ["Hello World"],
-        "mediaPaths": ["./test.png"]
+        "mediaPaths": ["./__tests__/test.png"]
     })
