1fcbdb1e13f97e4f2a4c6b7c5cfa09e5a744d986
Julian de Mourgues <juliandemourgues@gmail.com>
2024-01-13 17:03:10 +0100

    upload

--- etl/load/upload-s3/index.js
+++ etl/load/upload-s3/index.js
@@ -1,45 +1,55 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
+const { fromEnv } = require("@aws-sdk/credential-provider-env");
 const fs = require("fs");
 
-const s3Client = new S3Client();
+// Function to upload data to S3
+async function handler(inputs) {
+  const s3Client = new S3Client({
+    credentials: fromEnv(),
+    region: process.env.AWS_REGION || "eu-central-1",
+  });
 
-async function uploadFile(bucketName, fileKey, filePath) {
-  try {
-    const fileContent = fs.readFileSync(filePath);
+  const { bucket_name, files } = inputs;
+  let success = true;
 
-    const putObjectParams = {
-      Bucket: bucketName,
-      Key: fileKey,
-      Body: fileContent,
+  for (const file of files) {
+    const params = {
+      Bucket: bucket_name,
+      Key: file.key,
     };
 
-    const data = await s3Client.send(new PutObjectCommand(putObjectParams));
-    console.log(`File uploaded successfully: ${fileKey}`, data);
-  } catch (err) {
-    console.error("Error", err);
+    try {
+      if (file.content) {
+        params.Body = file.content;
+        await s3Client.send(new PutObjectCommand(params));
+      } else if (file.path) {
+        const upload = new Upload({
+          client: s3Client,
+          params: { ...params, Body: fs.createReadStream(file.path) },
+        });
+
+        await upload.done();
+      } else {
+        success = false;
+        continue;
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      success = false;
+    }
   }
+
+  return { success };
 }
 
-async function uploadMultipleFiles(bucketName, files) {
-  for (const file of files) {
-    await uploadFile(bucketName, file.key, file.path);
-  }
-}
+// Sample function call (commented out)
+// handler({
+//   bucket_name: 'your-bucket-name',
+//   files: [
+//     { content: 'Hello, world!', key: 'example1.txt' },
+//     { path: '/path/to/file.txt', key: 'example2.txt' }
+//   ]
+// });
 
-export const handler = async (event) => {
-  const bucketName = event.bucketName;
-  const filesToUpload = event.files;
-
-  try {
-    await uploadMultipleFiles(bucketName, filesToUpload);
-  } catch (err) {
-    console.error(err);
-    return { success: false };
-  }
-  return { success: true };
-};
-
-const bucketName = "your-bucket-name";
-const filesToUpload = ["path/to/local/file1.txt", "path/to/local/file2.txt"];
-
-// handler({ bucketName, filesToUpload });
+module.exports = { handler };
