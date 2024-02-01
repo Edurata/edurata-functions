7f6932b89f29adeea9076d2f04ab1b45437e90ea
Julian de Mourgues <juliandemourgues@gmail.com>
2024-02-02 00:48:58 +0100

    version

--- etl/extract/clone-git/index.js
+++ etl/extract/clone-git/index.js
@@ -1,4 +1,5 @@
 const simpleGit = require("simple-git");
+const fs = require("fs");
 const path = require("path");
 
 // Handler function
@@ -11,20 +12,32 @@ async function handler(inputs) {
     privateToken,
   } = inputs;
 
-  // Setting up the Git client
-  const git = simpleGit();
-  if (privateToken) {
-    // Configure Git for HTTPS token-based authentication
-    git
-      .env("GIT_TERMINAL_PROMPT", "0")
-      .env("GIT_ASKPASS", "echo")
-      .env("GIT_HTTPS_USERNAME", privateUser)
-      .env("GIT_HTTPS_PASSWORD", privateToken);
+  // Determine the local path for the repo
+  const localPath = `./cloned_repos/${path.basename(repoUrl)}`;
+
+  // Check if the repository already exists at the local path
+  if (fs.existsSync(localPath)) {
+    console.log(`Repository already exists at ${localPath}.`);
+    // Additional logic can be added here if you want to pull updates or handle this scenario differently
+  } else {
+    // Setting up the Git client
+    const git = simpleGit();
+    if (privateToken) {
+      // Configure Git for HTTPS token-based authentication
+      git
+        .env("GIT_TERMINAL_PROMPT", "0")
+        .env("GIT_ASKPASS", "echo")
+        .env("GIT_HTTPS_USERNAME", privateUser)
+        .env("GIT_HTTPS_PASSWORD", privateToken);
+    }
+
+    // Clone and checkout
+    await git.clone(repoUrl, localPath);
+    console.log(`Repository cloned to ${localPath}.`);
   }
 
-  // Clone and checkout
-  const localPath = `./cloned_repos/${path.basename(repoUrl)}`;
-  await git.clone(repoUrl, localPath);
+  // Change working directory to the local path
+  const git = simpleGit(localPath);
   await git.cwd(localPath);
   await git.checkout(ref);
 
@@ -40,6 +53,7 @@ async function handler(inputs) {
 //   repoUrl: 'https://github.com/user/repo.git',
 //   path: 'subdirectory',
 //   ref: 'main',
+//   privateUser: 'username',
 //   privateToken: 'your-token'
 // }).then(console.log);
 
