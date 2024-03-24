8669df053c17a3b36e8dba1a9cf413d656ca3572
Julian de Mourgues <juliandemourgues@gmail.com>
2024-03-24 11:39:11 +0100

    git headless

--- etl/extract/clone-git/index.js
+++ etl/extract/clone-git/index.js
@@ -1,43 +1,38 @@
-const nodegit = require("nodegit");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
+const { clone, checkout, init } = require("isomorphic-git");
+const http = require("isomorphic-git/http/node");
 
-/**
- * Clones a Git repository to a temporary directory, supports private repositories, and checks out a specified sub-path and ref.
- * If no sub-path is specified, defaults to the root of the repository.
- * @param {Object} inputs - The inputs for the function.
- * @param {string} inputs.repoUrl - The URL of the Git repository to clone.
- * @param {string} [inputs.privateToken] - A token for accessing private repositories.
- * @param {string} [inputs.path] - A sub-path within the repository to return. Defaults to the root of the repository.
- * @param {string} [inputs.ref] - A reference within the repository to checkout.
- * @returns {Object} The output of the function.
- */
 async function handler(inputs) {
   const tempDir = process.env.TMP_DIR || "/tmp";
-  const cloneOptions = {};
-  if (inputs.privateToken) {
-    cloneOptions.fetchOpts = {
-      callbacks: {
-        credentials: () =>
-          nodegit.Cred.userpassPlaintextNew(
-            inputs.privateToken,
-            "x-oauth-basic"
-          ),
-      },
-    };
-  }
   const repoPath = path.join(tempDir, `repo-${Date.now()}`);
+
   try {
-    await nodegit.Clone(inputs.repoUrl, repoPath, cloneOptions);
+    // Initialize an empty git repository
+    await init({ fs, dir: repoPath });
+
+    // Clone the repository
+    await clone({
+      fs,
+      dir: repoPath,
+      url: inputs.repoUrl,
+      singleBranch: true,
+      http,
+      depth: 1,
+      onAuth: (url, auth) => {
+        if (inputs.privateToken) {
+          return { ...auth, username: inputs.privateToken };
+        }
+      },
+    });
+
     const finalPath = inputs.path ? path.join(repoPath, inputs.path) : repoPath;
+
     // If a ref is specified, attempt to checkout that ref.
     if (inputs.ref) {
-      const repo = await nodegit.Repository.open(repoPath);
-      const reference = await repo.getBranchCommit(inputs.ref);
-      await nodegit.Checkout.tree(repo, reference, {
-        checkoutStrategy: nodegit.Checkout.STRATEGY.FORCE,
-      });
+      await checkout({ fs, dir: repoPath, ref: inputs.ref });
     }
+
     return { status: "Repository cloned successfully.", repoCode: finalPath };
   } catch (error) {
     console.error("Error cloning repository:", error);
