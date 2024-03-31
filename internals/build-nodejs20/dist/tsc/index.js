a3b41de86e61bbcd2786f6e9e51e63f7ed42d3b1
Julian de Mourgues <juliandemourgues@gmail.com>
2024-03-31 13:33:50 +0200

    foreach

--- internals/build-nodejs20/dist/tsc/index.js
+++ internals/build-nodejs20/dist/tsc/index.js
@@ -12,6 +12,7 @@ const handler = async (inputs) => {
     // Assuming codePath is a directory containing package.json
     try {
         // Synchronously execute npm install in the codePath directory
+        console.log("npm installing in ", codePath);
         (0, child_process_1.execSync)("npm install", { cwd: codePath, stdio: "inherit" });
         console.log("NPM modules installed successfully.");
     }
