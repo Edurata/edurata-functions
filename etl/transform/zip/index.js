68a2cc91b7ddb144f73d4a0d608d82433e0bbf46
Julian de Mourgues <juliandemourgues@gmail.com>
2024-03-31 18:34:19 +0200

    foreach

--- etl/transform/zip/index.js
+++ etl/transform/zip/index.js
@@ -1,27 +1,40 @@
 const fs = require("fs");
+const path = require("path");
 const archiver = require("archiver");
 
 // Handler function
 async function handler(inputs) {
-  const { file } = inputs;
-  const output = fs.createWriteStream(`${file}.zip`);
+  const { file: filePath } = inputs;
+  const output = fs.createWriteStream(`${filePath}.zip`);
   const archive = archiver("zip", {
     zlib: { level: 9 }, // Compression level
   });
 
   return new Promise((resolve, reject) => {
-    archive.on("error", (err) => reject(err)).pipe(output);
+    output.on("close", () => resolve({ zipped: `${filePath}.zip` }));
+    output.on("error", (err) => reject(err));
 
-    archive.file(file, { name: file.name });
+    archive.pipe(output);
+
+    if (fs.statSync(filePath).isFile()) {
+      // If the provided path is a file, add it to the archive
+      archive.file(filePath, { name: path.basename(filePath) });
+    } else {
+      // If the provided path is a directory, add all files and directories in it to the archive
+      const items = fs.readdirSync(filePath);
+      items.forEach((item) => {
+        const itemPath = path.join(filePath, item);
+        archive.directory(itemPath, path.basename(itemPath));
+      });
+    }
+
     archive.finalize();
-
-    output.on("close", () => resolve({ zipped: `${file}.zip` }));
   });
 }
 
 /* Example function call (commented out)
-handler({ file: { path: 'path/to/your/file', name: 'yourfile.txt' }})
-  .then((output) => console.log('Zipped file:', output.zipped))
+handler({ filePath: 'path/to/your/fileOrFolder' })
+  .then((output) => console.log('Zipped:', output.zipped))
   .catch((error) => console.error(error));
 */
 
