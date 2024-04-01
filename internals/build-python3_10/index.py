d7707626d22cef24abd44bbdd564c267cc0a4975
Julian de Mourgues <juliandemourgues@gmail.com>
2024-04-01 15:18:39 +0200

    foreach

--- internals/build-python3_10/index.py
+++ internals/build-python3_10/index.py
@@ -7,7 +7,7 @@ def handler(inputs):
     # Assuming codePath is a directory containing requirements.txt
     try:
         # Synchronously execute pip install in the codePath directory
-        subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', '-r', os.path.join(codePath, 'requirements.txt')])
+        subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', '-r', os.path.join(codePath, 'requirements.txt'), '-t', 'python_modules'])
         print('Python packages installed successfully.')
     except subprocess.CalledProcessError as error:
         print('Error installing Python packages:', error)
