801f4a59e57fcfdb5dd7fb2672388d7d4c623c33
Julian de Mourgues <juliandemourgues@gmail.com>
2024-04-19 03:51:29 +0200

    add mysql

---
+++ general/mysql/index.py
@@ -0,0 +1,29 @@
+import mysql.connector
+from mysql.connector import Error
+import os
+import json
+
+def handler(inputs):
+    query = inputs['query']
+    connection_config = {
+        "host": os.getenv('MYSQL_HOST'),
+        "user": os.getenv('MYSQL_USER'),
+        "password": os.getenv('MYSQL_PASSWORD'),
+        "database": os.getenv('MYSQL_DATABASE')
+    }
+    try:
+        connection = mysql.connector.connect(**connection_config)
+        if connection.is_connected():
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return {"result": json.dumps(result)}
+    except Error as e:
+        return {"error": str(e)}
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+
+# Sample function call (commented out for reference)
+# print(handler({"query": "SELECT * FROM users"}))
