8af80dcd64752f1e26c60db6cf73c1fdf69bd713
Julian de Mourgues <juliandemourgues@edurata.com>
2025-07-09 22:23:16 +0200

    new

--- tests/node/dist/tsc/index.js
+++ tests/node/dist/tsc/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.handler = void 0;
 const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const sleep = (ms) => {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
@@ -18,16 +19,17 @@ const handler = async (inputs) => {
             : inputs.sleepTime
         : 1000;
     const _message = inputs.message;
-    const _random_file_path = process.env.DEPLOYMENT_ID + ".txt";
-    const _filePath = inputs.infile || "testFile.txt";
+    const _random_file_path = path_1.default.join("/tmp", process.env.DEPLOYMENT_ID + ".txt");
+    const _filePath = path_1.default.join("/tmp", inputs.infile || "testFile.txt");
     console.log(_message);
     // print
     await sleep(_sleepTime);
     // Create dummy file
     fs_1.default.writeFileSync(_random_file_path, "Dummy file");
     if (inputs.infile) {
-        const fileData = fs_1.default.readFileSync(inputs.infile);
-        fs_1.default.writeFileSync(inputs.infile, fileData + _message);
+        const inputFilePath = path_1.default.join("/tmp", inputs.infile);
+        const fileData = fs_1.default.readFileSync(inputFilePath);
+        fs_1.default.writeFileSync(inputFilePath, fileData + _message);
     }
     else {
         fs_1.default.writeFileSync(_filePath, _message);
